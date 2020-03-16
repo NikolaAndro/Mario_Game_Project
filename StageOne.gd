2ea370c78948a759c79222517d5b35783f7d6c92
mgefdb <61481155+mgefdb@users.noreply.github.com>
2020-03-16 00:25:09 -0500

    Add files via upload

--- StageOne.gd
+++ StageOne.gd
@@ -3,11 +3,14 @@ extends Node2D
 var path = "res://Flat_World.csv"
 var counter = 300
 
-func _ready():
-	$player/VBoxContainer/Current_Time.text = str(counter)
+func ready():
+	$player/HBoxContainer/Time/Current_Time.text = str(counter)
+	$player/HBoxContainer/World/Current_World.text = 1-1
+	$player/HBoxContainer/Lives/Current_Lives.text = $player.lives
+	$player/HBoxContainer/Score/Current_Score.text = $player.score
+	$player/HBoxContainer/Coins/Current_Coins.text = $player.coins
 	MusicController.play("res://music/Super_Mario_Bros_Music.ogg")
 	
 func _on_Timer_timeout():
 	counter -= 1
-	$player/VBoxContainer/Current_Time.text = str(counter)		
-
+	$player/HBoxContainer/Time/Current_Time.text = str(counter)		
